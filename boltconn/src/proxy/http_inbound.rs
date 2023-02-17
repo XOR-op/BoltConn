316da05942ef109c0db5fe8a80ebc343521e44f6
XOR-op <17672363+XOR-op@users.noreply.github.com>
2023-02-17 23:12:00 +0800

    feature: http inbound auth

--- boltconn/src/proxy/http_inbound.rs
+++ boltconn/src/proxy/http_inbound.rs
@@ -1,5 +1,6 @@
 use crate::proxy::Dispatcher;
 use anyhow::anyhow;
+use base64::Engine;
 use httparse::Request;
 use std::io;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
@@ -10,7 +11,7 @@ use tokio::net::{TcpListener, TcpStream};
 
 pub struct HttpInbound {
     server: TcpListener,
-    auth: Option<(String, String)>,
+    auth: Option<String>,
     dispatcher: Arc<Dispatcher>,
 }
 
@@ -24,7 +25,7 @@ impl HttpInbound {
             TcpListener::bind(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port)).await?;
         Ok(Self {
             server,
-            auth,
+            auth: auth.map(|(usr, pwd)| usr + ":" + pwd.as_str()),
             dispatcher,
         })
     }
@@ -32,12 +33,14 @@ impl HttpInbound {
     pub async fn run(self) {
         while let Ok((socket, addr)) = self.server.accept().await {
             let disp = self.dispatcher.clone();
-            tokio::spawn(Self::serve_connection(socket, addr, disp));
+            let auth = self.auth.clone();
+            tokio::spawn(Self::serve_connection(socket, auth, addr, disp));
         }
     }
 
     async fn serve_connection(
         socket: TcpStream,
+        auth: Option<String>,
         addr: SocketAddr,
         dispatcher: Arc<Dispatcher>,
     ) -> anyhow::Result<()> {
@@ -53,21 +56,52 @@ impl HttpInbound {
             }
         }
         let mut socket = buf_reader.into_inner();
-        let mut buf = [httparse::EMPTY_HEADER; 1];
+        let mut buf = [httparse::EMPTY_HEADER; 16];
         let mut req_struct = Request::new(buf.as_mut());
         req_struct.parse(req.as_bytes())?;
         if req_struct.method.map_or(false, |m| m == "CONNECT")
             // HTTP/1.1
             && req_struct.version.map_or(false, |v| v == 1)
         {
-            if let Some(path) = req_struct.path {
-                if let Ok(dest) = path.parse() {
-                    socket.write_all(Self::response200().as_bytes()).await?;
-                    dispatcher
-                        .submit_tun_tcp(addr, dest, Arc::new(AtomicU8::new(2)), socket)
-                        .await;
-                    return Ok(());
+            if let Some(Ok(dest)) = req_struct.path.map(|p| p.parse()) {
+                let authorized = if let Some(auth) = auth {
+                    // let's verify the auth
+                    let mut r = false;
+                    for hdr in req_struct.headers.iter() {
+                        if hdr.name.eq_ignore_ascii_case("proxy-authorization") {
+                            let Ok(value) = std::str::from_utf8(hdr.value)else{
+                                break;
+                            };
+                            // manually split
+                            if value.is_ascii() && value.len() > 6 {
+                                let (left, right) = value.split_at(6);
+                                if left.eq_ignore_ascii_case("basic ") {
+                                    let b64decoder = base64::engine::general_purpose::STANDARD;
+                                    if let Ok(code) = b64decoder.decode(right) {
+                                        if std::str::from_utf8(code.as_slice())
+                                            .map(|s| s == auth.as_str())
+                                            == Ok(true)
+                                        {
+                                            r = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    r
+                } else {
+                    true
+                };
+                if !authorized {
+                    socket.write_all(Self::response403().as_bytes()).await?;
+                    return Err(anyhow!("Invalid CONNECT request"));
                 }
+                socket.write_all(Self::response200().as_bytes()).await?;
+                dispatcher
+                    .submit_tun_tcp(addr, dest, Arc::new(AtomicU8::new(2)), socket)
+                    .await;
+                return Ok(());
             }
         }
         socket.write_all(Self::response403().as_bytes()).await?;
