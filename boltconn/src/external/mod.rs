3534f9f01fde499c1293972cedeb4af0b94ea570
XOR-op <17672363+XOR-op@users.noreply.github.com>
2023-06-11 21:55:39 +0800

    WIP: try tarpc

--- boltconn/src/external/mod.rs
+++ boltconn/src/external/mod.rs
@@ -1,9 +1,11 @@
 mod controller;
 mod database;
 mod logger;
+mod uds_controller;
 mod web_controller;
 
 pub use controller::*;
 pub use database::*;
 pub use logger::*;
+pub use uds_controller::*;
 pub use web_controller::*;
