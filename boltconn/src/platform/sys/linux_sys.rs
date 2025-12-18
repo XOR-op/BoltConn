bfb215a4c77f3151ff5e38ad6cf06b80d340ee89
XOR-op <17672363+XOR-op@users.noreply.github.com>
2025-12-18 14:49:01 -0500

    lints: fix clippy

--- boltconn/src/platform/sys/linux_sys.rs
+++ boltconn/src/platform/sys/linux_sys.rs
@@ -16,25 +16,27 @@ use std::{cmp, io, mem};
 use super::super::errno_err;
 
 pub unsafe fn open_tun() -> io::Result<(i32, String)> {
-    let mut req: ifreq = mem::zeroed();
-    req.ifru.flags = IFF_TUN | IFF_NO_PI;
-    let fd = {
-        let fd = libc::open(c"/dev/net/tun".as_ptr() as *const _, O_RDWR);
-        if fd < 0 {
-            return Err(errno_err("Failed to open /dev/net/tun"));
+    unsafe {
+        let mut req: ifreq = mem::zeroed();
+        req.ifru.flags = IFF_TUN | IFF_NO_PI;
+        let fd = {
+            let fd = libc::open(c"/dev/net/tun".as_ptr() as *const _, O_RDWR);
+            if fd < 0 {
+                return Err(errno_err("Failed to open /dev/net/tun"));
+            }
+            fd
+        };
+        if tunsetiff(fd, &mut req as *mut _ as *mut _) < 0 {
+            libc::close(fd);
+            return Err(errno_err("Failed to tunsetiff"));
         }
-        fd
-    };
-    if tunsetiff(fd, &mut req as *mut _ as *mut _) < 0 {
-        libc::close(fd);
-        return Err(errno_err("Failed to tunsetiff"));
+        Ok((
+            fd,
+            CStr::from_ptr(req.ifrn.name.as_ptr())
+                .to_string_lossy()
+                .into_owned(),
+        ))
     }
-    Ok((
-        fd,
-        CStr::from_ptr(req.ifrn.name.as_ptr())
-            .to_string_lossy()
-            .into_owned(),
-    ))
 }
 
 fn ip_command_by_addr(addr: &IpAddr) -> Command {
